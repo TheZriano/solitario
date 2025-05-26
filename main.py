a701cf083043d80e2213410b0eef853d8eb2f282
Giacomo Montenegro <giacomo.montenegro09@gmail.com>
2025-05-26 10:50:19 +0200

    aggiunta funzione di draw

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+import random
 
 
 class Card:
@@ -7,42 +8,44 @@ class Card:
         self.seed=seed
         self.show=show
 
+seeds=["♠","♡","♣","♢"]
+values=["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
+
+
 deck=[]
+scrapsDeck=[]
+
 finaldecks=[[],[],[],[]]
 
 tableau=[
-    [Card(0,3),Card(1,2,show=True),Card(11,0,show=True)],
-    [Card(0,3),Card(1,2,show=True),Card(11,0,show=True)],
     [],
-    [Card(0,3),Card(1,2,show=True),Card(11,0,show=True)],
-    [Card(0,3),Card(1,2,show=True),Card(11,0,show=True)],
-    [Card(0,3),Card(1,2,show=True),Card(11,0,show=True)],
+    [],
+    [],
+    [],
+    [],
+    [],
     []
     ]
 
-hand=[Card(0,3),Card(1,2),Card(11,0)]
-seeds=["♠","♡","♣","♢"]
-values=["A","1","2","3","4","5","6","7","8","9","10","J","Q","K"]
+hand=[]
 
 
 
-def screenRefresh():
+
+
+def screenRefresh(message=""):
     os.system("cls")
-    print(""" SOLITARIO """) #Titolo della pagina
+    print(f""" SOLITARIO \n\nFai la tua mossa!\n{message}\n """) #Titolo della pagina
 
     #disegno deck + mano
-    print(f"""Mazzo:  Mano:
-______  ______  ______  ______
-|▒▒▒▒|  | {values[hand[0].value]}  |  | {values[hand[1].value]}  |  | {values[hand[2].value]}  |
-|▒▒▒▒|  |    |  |    |  |    |
-|▒▒▒▒|  | {seeds[hand[0].seed]}  |  | {seeds[hand[1].seed]}  |  | {seeds[hand[2].seed]}  |
-¯¯¯¯¯¯  ¯¯¯¯¯¯  ¯¯¯¯¯¯  ¯¯¯¯¯¯""")
+    print(writeDeckAndHand())
     
     #4 mazzi finali
-    print("Scale:")
+    print("\nScale:")
     print(writeFinalDecks())
 
     #tableau 7 mazzi
+    print("Tableau:")
     print(writeTableau())
 
 
@@ -55,7 +58,7 @@ ______  ______  ______  ______\n"""
             continue
         else:
             card=deck[-1]
-            string+=f"|{values[card.value]}  {seeds[card.seed]}|  "
+            string+=f"|{values[card.value]+(' ' if card.value!=9 else '')} {seeds[card.seed]}|  "
     string+="\n|    |  |    |  |    |  |    |\n"
     for deck in finaldecks:
         if deck==[]:
@@ -63,10 +66,9 @@ ______  ______  ______  ______\n"""
             continue
         else:
             card=deck[-1]
-            string+=f"|{seeds[card.seed]}  {values[card.value]}|  "
+            string+=f"|{seeds[card.seed]} {(' ' if card.value!=9 else '')+ values[card.value]}|  "
     string+="\n¯¯¯¯¯¯  ¯¯¯¯¯¯  ¯¯¯¯¯¯  ¯¯¯¯¯¯\n"
     return string
-
 
 def writeTableau():
     rows=[
@@ -91,7 +93,7 @@ def writeTableau():
             for card in deck:
                 if card.show:
                     rows[1]+="___"
-                    rows[2]+=f"|{values[card.value]} "
+                    rows[2]+=f"|{values[card.value]+(' ' if card.value!=9 else '')}"
                     rows[3]+="|  "
                     rows[4]+=f"|{seeds[card.seed]} "
                     rows[5]+="¯¯¯"
@@ -105,7 +107,7 @@ def writeTableau():
             rows[1]+="___  "
             rows[2]+=f" {seeds[card.seed]}|  "
             rows[3]+="  |  "
-            rows[4]+=f" {values[card.value]}|  "
+            rows[4]+=f"{(' ' if card.value!=9 else '')+ values[card.value]}|  "
             rows[5]+="¯¯¯  "
 
         
@@ -113,9 +115,78 @@ def writeTableau():
     string="\n".join(rows)
     return string
                 
+def writeDeckAndHand():
+    rows=[
+        "Mazzo:  Mano:",
+        "______  ",
+        "|▒▒▒▒|  ",
+        "|▒▒▒▒|  ",
+        "|▒▒▒▒|  ",
+        "¯¯¯¯¯¯  ",
+        ]
+    for card in hand:
+        if card.show:
+            rows[1]+="___"
+            rows[2]+=f"|{values[card.value]+(' ' if card.value!=9 else '')}"
+            rows[3]+="|  "
+            rows[4]+=f"|{seeds[card.seed]} "
+            rows[5]+="¯¯¯"
+        else:
+            rows[1]+="___"
+            rows[2]+=f"|▒▒"
+            rows[3]+="|▒▒"
+            rows[4]+=f"|▒▒"
+            rows[5]+="¯¯¯"
+
+    rows[1]+="___  "
+    rows[2]+=f" {seeds[card.seed]}|  "
+    rows[3]+="  |  "
+    rows[4]+=f"{(' ' if card.value!=9 else '')+ values[card.value]}|  "
+    rows[5]+="¯¯¯  "
+
+    return "\n".join(rows)
+
+def startGame():
+    for seed in range(4):
+        for value in range(13):
+            deck.append(Card(value,seed))
+    
+    random.shuffle(deck)
+    for i in range(7):
+        for c in range(i+1):
+            tableau[i].append(deck[-1])
+            deck.pop(-1)
+        tableau[i][-1].show=True
+
+    hand.append(deck[-1])
+    deck.pop(-1)
+    hand[0].show=True
+    screenRefresh()
 
 
+startGame()
 
-screenRefresh()
+gameOn=True
+while gameOn:
+    command=input(">>>  ").split(" ")
+    match command[0]:
+        case "draw":
+            hand.append(deck[-1])
+            hand[-1].show=True
+            deck.pop(-1)
+            if len(deck)==0:
+                random.shuffle(scrapsDeck)
+                deck=scrapsDeck.copy()
+                scrapsDeck=[]
+            if len(hand)>3:
+                hand[0].show=False
+                scrapsDeck.append(hand[0])
+                hand.pop(0)
 
-input(">>>  ")
+        case "move":
+            pass
+
+        case _:
+            screenRefresh("Mossa non valida!!")
+            continue
+    screenRefresh()
